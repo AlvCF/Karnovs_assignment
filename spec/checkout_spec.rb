f0f90b6320343669caf283c5764e1c3cbf5e3978
Alvaro Cuesta <alvalau_5@hotmail.com>
2023-04-17 20:36:50 +0200

    Tweaked the skeleton test

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -6,26 +6,17 @@ require "promotion"
 require 'promotion_total'
 require 'promotion_product'
 
-RSpec.describe Checkout, '#calculate' do
-  context 'Scan item' do
-    it 'should be correct' do
-      first_promotion, second_promotion, red_scarf, silver_cufflinks,silk_dress = prepare_data
-      first_checkout = described_class.new([first_promotion,second_promotion])
-      first_checkout.scan(red_scarf)
-      expect(first_checkout.total).to eq(9.25)
+describe Checkout do
+  let(:red_scarf) { Product.new("001","Red Scarf",9.25) }
+  let(:first_promotion) { PromotionProduct.new(false,0.75,"001",2) }
+  let(:new_checkout) { Checkout.new([:first_promotion])}
+
+  describe '#checkout' do
+    context 'when a checkout scans an item' do
+      it 'applies promotions and updates the checkouts total' do
+        expect{ new_checkout.scan(red_scarf)}.to change(new_checkout, :total).by(9.25)
+      end
     end
   end
 
-end
-
-def prepare_data
-  red_scarf         = Product.new("001","Red Scarf",9.25)
-  silver_cufflinks  = Product.new("002","Silver cufflinks",45.0)
-  silk_dress        = Product.new("003","Silk Dress",19.95)
-
-
-  first_promotion   = PromotionProduct.new(false,0.75,"001",2)
-  second_promotion  = PromotionTotal.new(true,0.1,60)
-
-  [first_promotion, second_promotion, red_scarf, silver_cufflinks,silk_dress]
 end
